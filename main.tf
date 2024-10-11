bc37f288022ebd8e0e51a282f1c5b53947427bae
vroniu <gwrona1@gmail.com>
2024-10-11 14:21:18 +0200

    Create a VPC for the instances

--- main.tf
+++ main.tf
@@ -19,9 +19,53 @@ provider "aws" {
     region = "eu-central-1"
 }
 
+// Set up a VPC with a public subnet for instances
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/28"
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "VPC for Task 2"
+    }
+}
+
+resource "aws_subnet" "public-subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/28"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "Public Subnet for Task 2"
+    }
+}
+
+// Connect the public subnet to the internet via Internet Gateway
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
+    
+    tags = {
+        Name = "Internet Gateway for Task 2"
+    }
+}
+
+resource "aws_route_table" "internet-access-route" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+}
+
+resource "aws_route_table_association" "public-subnet-internet-gateway-connection" {
+    subnet_id = aws_subnet.public-subnet.id
+    route_table_id = aws_route_table.internet-access-route.id
+}
+
 // Security group that allows access in/out
 resource "aws_security_group" "sg-allow-internet-access" {
     name = "allow-internet-access"
+    vpc_id = aws_vpc.main.id
 
     egress {
         from_port = 0
@@ -54,7 +98,6 @@ resource "aws_security_group" "sg-allow-internet-access" {
         protocol = "icmp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
 }
 
 // Set up key-pair to log in to the instances via SSH
@@ -77,6 +120,8 @@ resource "local_file" "ssh-keypair-private-key" {
 resource "aws_instance" "amazon-linux-instance" {
     instance_type = "t2.micro"
     ami = "ami-0592c673f0b1e7665"
+    subnet_id = aws_subnet.public-subnet.id
+    key_name = "ssh-keypair"
 
     tags = {
         Name = "amazon-linux-instance"
@@ -86,6 +131,7 @@ resource "aws_instance" "amazon-linux-instance" {
 resource "aws_instance" "ubuntu-instance" {
     instance_type = "t2.micro"
     ami = "ami-0084a47cc718c111a"
+    subnet_id = aws_subnet.public-subnet.id
     vpc_security_group_ids = [aws_security_group.sg-allow-internet-access.id]
     key_name = "ssh-keypair"
 
